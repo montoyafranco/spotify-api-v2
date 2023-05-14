92da55087d0ea5961f50f94818fab4405e7c22d3
montoyafranco <francomontoya9810@gmail.com>
2023-05-14 20:33:19 -0300

    funcionando el buscador y regeneracion de token

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { finalize, map } from 'rxjs';
 import { RequestService } from 'src/app/services/request.service';
 
 @Component({
@@ -9,28 +10,54 @@ import { RequestService } from 'src/app/services/request.service';
 export class HomeComponent {
   constructor(private request: RequestService) {}
 
-  private readonly accessTokenKey = 'spotifyToken';
+
   private readonly expiresIn = 3600;
   ngOnInit() {
-    console.log(localStorage.getItem('token'));
+    console.log(
+      'este es el token de inicio de sesion de usuario ---------------- \n \n' +
+        localStorage.getItem('token')
+    );
     this.getToken();
+    this.request.getArtist().subscribe((data) => console.log(data));
+    this.crearTokenSpotify();
+
+    
   }
 
   getToken() {
-    const cachedToken = localStorage.getItem(this.accessTokenKey);
-    console.log(cachedToken);
-    if (cachedToken) {
-      const { token, expiration } = JSON.parse(cachedToken);
-      if (expiration > Date.now()) {
-        // Token is still valid, return it
-        console.log(token);
-        return token;
-      }
-      // Token has expired, remove it from cache
-      localStorage.removeItem(this.accessTokenKey);
-    }
-    this.request.getTokenSpotify().subscribe((data) => {
-      console.log(data);
-    });
+    const cachedToken = localStorage.getItem('spotifyToken');
+    console.log(
+      ' hola soy token cacheado de spotify ------------ \n\n' + cachedToken
+    );
+
+    // this.request.getTokenSpotify().subscribe((data) => {
+    //   console.log(data);
+    // });
+  }
+  crearTokenSpotify() {
+    if(!localStorage.getItem("spotifyToken")){
+      this.request.firstTimeSpotifyToken().subscribe((data) => console.log(data))
+    }   
+   
+ 
+   
+ }
+
+  public isLoading = false;
+  public src: string | undefined;
+  public data$: any;
+
+  search(value: any): any {
+    this.isLoading = true;
+
+    this.data$ = this.request.searchTrack({ q: value }).pipe(
+      map(function ({ tracks }) {
+        console.log(tracks.items)
+        return tracks.items;
+      }),
+      finalize(() => (this.isLoading = false))
+    );
   }
 }
+ 
+
--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, catchError, map, of, tap } from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, catchError, map, of, tap, throwError } from 'rxjs';
 import { AuthResponse, User } from '../interfaces/User';
 
 @Injectable({
@@ -10,54 +10,87 @@ import { AuthResponse, User } from '../interfaces/User';
 export class RequestService {
   createPostUrl: string = 'http://localhost:8080/api/user/register';
   BASE_URL: string = 'http://localhost:8080/api/user/login';
+  Buscar: string = 'https://api.spotify.com/v1/search';
+
+  private headerCustom!: HttpHeaders;
 
   constructor(private http: HttpClient) {}
 
-  private httpOptionsAuthorization = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      Authorization: localStorage.getItem('token') ?? '',
-    }),
-  };
-
-  public getTokenSpotify() {
-    const token = localStorage.getItem('spotifyToken');
-
-    if (token) {
-      // El token ya está en localStorage, devolverlo como un Observable
-      return of(token);
-    } else {
-      // El token no está en localStorage, enviar una solicitud POST para obtener uno nuevo
-      const body = new URLSearchParams();
-      body.set('grant_type', 'client_credentials');
-      body.set('client_id', '87d9838110b7473d816cd83e0a842bfd');
-      body.set('client_secret', '92a8c0f286bc4db4beceb42c036b132a');
-
-      const headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      };
-
-      return this.http
-        .post('https://accounts.spotify.com/api/token', body.toString(), {
-          headers,
-        })
-        .pipe(
-          tap((response: any) => {
-            // Almacenar el token en localStorage y establecer una hora de expiración de 1 hora
-            const expirationTime = Date.now() + response.expires_in * 1000;
-            localStorage.setItem('spotifyToken', "Bearer "+ response.access_token);
-            localStorage.setItem(
-              'spotifyTokenExpiration',
-              expirationTime.toString()
-            );
-          }),
-          map((response: any) => {
-            // Devolver el token como un Observable
-            return response.access_token;
-          })
-        );
-    }
+  public searchTrack({ q }: TrackModel): Observable<any> {
+    this.headerCustom = new HttpHeaders({
+      Authorization: `${localStorage.getItem('spotifyToken')}`,
+    });
+    return this.http.get(`${this.Buscar}?q=${q}&type=track&limit=10`, {
+      headers: this.headerCustom,
+    }).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          this.handleUnauthorizedError();
+        }
+        return throwError(error);
+      }),
+    );
   }
+
+  public handleUnauthorizedError(){
+    localStorage.removeItem('spotifyToken')
+    this.firstTimeSpotifyToken();
+
+  }
+
+  public getArtist() {
+    let token = localStorage.getItem('spotifyToken');
+    console.log('Soy el log de GetArtis en service \n\n' + token);
+
+    let headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: `${token}`,
+    };
+
+    return this.http.get(
+      'https://api.spotify.com/v1/search?q=remaster%2520track%3ADoxy%2520artist%3AMiles%2520Davis&type=album',
+      { headers }
+    );
+  }
+
+ 
+  public firstTimeSpotifyToken() {   
+    const body = new URLSearchParams();
+    body.set('grant_type', 'client_credentials');
+    body.set('client_id', '87d9838110b7473d816cd83e0a842bfd');
+    body.set('client_secret', '92a8c0f286bc4db4beceb42c036b132a');
+  
+    const headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    };
+  
+    return this.http
+      .post('https://accounts.spotify.com/api/token', body.toString(), {
+        headers,
+      })
+      .pipe(
+        tap((response: any) => {
+          // Almacenar el token en localStorage y establecer una hora de expiración de 1 hora
+          const expirationTime = Date.now() + response.expires_in * 1000;
+          localStorage.setItem(
+            'spotifyToken',
+            'Bearer ' + response.access_token
+          );
+          localStorage.setItem(
+            'spotifyTokenExpiration',
+            expirationTime.toString()
+          );
+        }),
+        map((response: any) => {
+          // Devolver el token como un Observable
+          return response.access_token;
+        })
+      );
+  }
+  
+  
+
+
 
   public createUser(user: User): Observable<User> {
     return this.http
@@ -85,4 +118,8 @@ export class RequestService {
         catchError(this.handleError<any>('Error '))
       );
   }
+}
+
+export class TrackModel {
+  q: string | undefined;
 }
