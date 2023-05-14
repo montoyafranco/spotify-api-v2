38b1fcebdf3f17036e45f2ecd5f266a54c915394
montoyafranco <francomontoya9810@gmail.com>
2023-05-14 17:19:50 -0300

    Obtener token de spotify automaticamente con nginit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
+import { RequestService } from 'src/app/services/request.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  ngOnInit(){
-    console.log(localStorage.getItem('token'))
+  constructor(private request: RequestService) {}
+
+  private readonly accessTokenKey = 'spotifyToken';
+  private readonly expiresIn = 3600;
+  ngOnInit() {
+    console.log(localStorage.getItem('token'));
+    this.getToken();
   }
 
+  getToken() {
+    const cachedToken = localStorage.getItem(this.accessTokenKey);
+    console.log(cachedToken);
+    if (cachedToken) {
+      const { token, expiration } = JSON.parse(cachedToken);
+      if (expiration > Date.now()) {
+        // Token is still valid, return it
+        console.log(token);
+        return token;
+      }
+      // Token has expired, remove it from cache
+      localStorage.removeItem(this.accessTokenKey);
+    }
+    this.request.getTokenSpotify().subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -7,9 +7,7 @@ import { AuthResponse, User } from '../interfaces/User';
 @Injectable({
   providedIn: 'root',
 })
-
 export class RequestService {
-  
   createPostUrl: string = 'http://localhost:8080/api/user/register';
   BASE_URL: string = 'http://localhost:8080/api/user/login';
 
@@ -18,12 +16,48 @@ export class RequestService {
   private httpOptionsAuthorization = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
-      'Authorization': localStorage.getItem('token') ?? ''
-    })
+      Authorization: localStorage.getItem('token') ?? '',
+    }),
   };
-  
 
- 
+  public getTokenSpotify() {
+    const token = localStorage.getItem('spotifyToken');
+
+    if (token) {
+      // El token ya está en localStorage, devolverlo como un Observable
+      return of(token);
+    } else {
+      // El token no está en localStorage, enviar una solicitud POST para obtener uno nuevo
+      const body = new URLSearchParams();
+      body.set('grant_type', 'client_credentials');
+      body.set('client_id', '87d9838110b7473d816cd83e0a842bfd');
+      body.set('client_secret', '92a8c0f286bc4db4beceb42c036b132a');
+
+      const headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      };
+
+      return this.http
+        .post('https://accounts.spotify.com/api/token', body.toString(), {
+          headers,
+        })
+        .pipe(
+          tap((response: any) => {
+            // Almacenar el token en localStorage y establecer una hora de expiración de 1 hora
+            const expirationTime = Date.now() + response.expires_in * 1000;
+            localStorage.setItem('spotifyToken', "Bearer "+ response.access_token);
+            localStorage.setItem(
+              'spotifyTokenExpiration',
+              expirationTime.toString()
+            );
+          }),
+          map((response: any) => {
+            // Devolver el token como un Observable
+            return response.access_token;
+          })
+        );
+    }
+  }
 
   public createUser(user: User): Observable<User> {
     return this.http
@@ -41,17 +75,14 @@ export class RequestService {
     };
   }
   public login(user: User): Observable<any> {
-    return this.http.post<any>(this.BASE_URL, user, {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-    }).pipe(
-      tap(response => console.log(response)),
-      map(response => response.token),
-      catchError(this.handleError<any>('Error '))
-    );
+    return this.http
+      .post<any>(this.BASE_URL, user, {
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      })
+      .pipe(
+        tap((response) => console.log(response)),
+        map((response) => response.token),
+        catchError(this.handleError<any>('Error '))
+      );
   }
-  
- 
-  
-  
-  
 }
